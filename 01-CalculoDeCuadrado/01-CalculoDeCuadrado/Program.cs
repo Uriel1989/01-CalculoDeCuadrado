ca88a8282c2bdcf32a357ca0a637a14e250eb288
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:12:20 -0300

    Add files via upload

---
+++ 01-CalculoDeCuadrado/01-CalculoDeCuadrado/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01_CalculoDeCuadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            b = 0;
+            Console.WriteLine("Ingrese un numero para calcular su cuadrado que sea mayor a cero:");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            while (a <= 0)
+            {
+                Console.WriteLine("Error, numero ingresado incorrecto, por favor reingrese:");
+                a = Convert.ToInt32(Console.ReadLine());
+
+            }
+            if (a > 0)
+            {
+                b = (a * a);
+            }
+            Console.WriteLine("El cuadrado del numero es: " + b);
+
+            Console.ReadKey();
+        }
+    }
+}
